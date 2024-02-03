52047508aa3b692cce0b30aa3f2415a00ae22bf9
Deepak Surya <ideepaksuryas@gmail.com>
2024-02-03 22:57:36 +0530

    add: prettier and eslint configs

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -1,3 +1,11 @@
-const Hello = (name?: string) => `${name || "World"}!`;
+const Hello = (name?: string) => `${name || 'World'}!`;
+
+// const user = {
+//   jobTitle: 'Software Engineer',
+// };
+
+// if ((user.jobTitle = 'Junior Engineer')) {
+//   user.jobTitle = 'Bug Engineer';
+// }
 
 export default Hello;
